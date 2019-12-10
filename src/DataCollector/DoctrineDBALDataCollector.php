300d236d10a8dd3ab39bfdd161d85b9ede5872c2
David Maicher <mail@dmaicher.de>
2019-12-10 21:11:14 +0100

    fix data collector

--- src/DataCollector/DoctrineDBALDataCollector.php
+++ src/DataCollector/DoctrineDBALDataCollector.php
@@ -3,12 +3,20 @@
 namespace Doctrine\Bundle\DBALBundle\DataCollector;
 
 use Doctrine\DBAL\Logging\DebugStack;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use Symfony\Bridge\Doctrine\DataCollector\ObjectParameter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollector;
 
 class DoctrineDBALDataCollector extends DataCollector
 {
+    /**
+     * @var string[]
+     */
+    private $groupedQueries;
+
     /**
      * @var DebugStack[]
      */
@@ -19,9 +27,26 @@ class DoctrineDBALDataCollector extends DataCollector
      */
     public function collect(Request $request, Response $response, \Throwable $exception = null)
     {
-        //TODO: re-implement based on collector from DoctrineBundle and Symfony DoctrineBridge
+        $queries = [];
+        foreach ($this->loggers as $name => $logger) {
+            $queries[$name] = $this->sanitizeQueries($name, $logger->queries);
+        }
 
-        $this->data = [];
+        $this->data = [
+            'queries' => $queries,
+        ];
+
+        // Might be good idea to replicate this block in doctrine bridge so we can drop this from here after some time.
+        // This code is compatible with such change, because cloneVar is supposed to check if input is already cloned.
+        foreach ($this->data['queries'] as &$queries) {
+            foreach ($queries as &$query) {
+                $query['params'] = $this->cloneVar($query['params']);
+                // To be removed when the required minimum version of symfony/doctrine-bridge is >= 4.4
+                $query['runnable'] = $query['runnable'] ?? true;
+            }
+        }
+
+        $this->groupedQueries   = null;
     }
 
     /**
@@ -51,5 +76,186 @@ class DoctrineDBALDataCollector extends DataCollector
     public function addLogger(string $name, DebugStack $logger)
     {
         $this->loggers[$name] = $logger;
+    }
+
+    public function getConnections()
+    {
+        return [];
+    }
+
+    public function getQueryCount()
+    {
+        return array_sum(array_map('count', $this->data['queries']));
+    }
+
+    public function getQueries()
+    {
+        return $this->data['queries'];
+    }
+
+    public function getTime()
+    {
+        $time = 0;
+        foreach ($this->data['queries'] as $queries) {
+            foreach ($queries as $query) {
+                $time += $query['executionMS'];
+            }
+        }
+
+        return $time;
+    }
+
+    public function getGroupedQueryCount()
+    {
+        $count = 0;
+        foreach ($this->getGroupedQueries() as $connectionGroupedQueries) {
+            $count += count($connectionGroupedQueries);
+        }
+
+        return $count;
+    }
+
+    public function getGroupedQueries()
+    {
+        if ($this->groupedQueries !== null) {
+            return $this->groupedQueries;
+        }
+
+        $this->groupedQueries = [];
+        $totalExecutionMS     = 0;
+        foreach ($this->data['queries'] as $connection => $queries) {
+            $connectionGroupedQueries = [];
+            foreach ($queries as $i => $query) {
+                $key = $query['sql'];
+                if (! isset($connectionGroupedQueries[$key])) {
+                    $connectionGroupedQueries[$key]                = $query;
+                    $connectionGroupedQueries[$key]['executionMS'] = 0;
+                    $connectionGroupedQueries[$key]['count']       = 0;
+                    $connectionGroupedQueries[$key]['index']       = $i; // "Explain query" relies on query index in 'queries'.
+                }
+                $connectionGroupedQueries[$key]['executionMS'] += $query['executionMS'];
+                $connectionGroupedQueries[$key]['count']++;
+                $totalExecutionMS += $query['executionMS'];
+            }
+            usort($connectionGroupedQueries, static function ($a, $b) {
+                if ($a['executionMS'] === $b['executionMS']) {
+                    return 0;
+                }
+
+                return $a['executionMS'] < $b['executionMS'] ? 1 : -1;
+            });
+            $this->groupedQueries[$connection] = $connectionGroupedQueries;
+        }
+
+        foreach ($this->groupedQueries as $connection => $queries) {
+            foreach ($queries as $i => $query) {
+                $this->groupedQueries[$connection][$i]['executionPercent'] =
+                    $this->executionTimePercentage($query['executionMS'], $totalExecutionMS);
+            }
+        }
+
+        return $this->groupedQueries;
+    }
+
+    private function executionTimePercentage($executionTimeMS, $totalExecutionTimeMS)
+    {
+        if ($totalExecutionTimeMS === 0.0 || $totalExecutionTimeMS === 0) {
+            return 0;
+        }
+
+        return $executionTimeMS / $totalExecutionTimeMS * 100;
+    }
+
+    private function sanitizeQueries(string $connectionName, array $queries): array
+    {
+        foreach ($queries as $i => $query) {
+            $queries[$i] = $this->sanitizeQuery($connectionName, $query);
+        }
+
+        return $queries;
+    }
+
+    private function sanitizeQuery(string $connectionName, array $query): array
+    {
+        $query['explainable'] = true;
+        $query['runnable'] = true;
+        if (null === $query['params']) {
+            $query['params'] = [];
+        }
+        if (!\is_array($query['params'])) {
+            $query['params'] = [$query['params']];
+        }
+        if (!\is_array($query['types'])) {
+            $query['types'] = [];
+        }
+        foreach ($query['params'] as $j => $param) {
+            $e = null;
+            if (isset($query['types'][$j])) {
+                // Transform the param according to the type
+                $type = $query['types'][$j];
+                if (\is_string($type)) {
+                    $type = Type::getType($type);
+                }
+                if ($type instanceof Type) {
+                    $query['types'][$j] = $type->getBindingType();
+                    try {
+                        //TODO: this needs a ConnectionRegistry
+                        //$param = $type->convertToDatabaseValue($param, $this->registry->getConnection($connectionName)->getDatabasePlatform());
+                    } catch (\TypeError $e) {
+                    } catch (ConversionException $e) {
+                    }
+                }
+            }
+
+            list($query['params'][$j], $explainable, $runnable) = $this->sanitizeParam($param, $e);
+            if (!$explainable) {
+                $query['explainable'] = false;
+            }
+
+            if (!$runnable) {
+                $query['runnable'] = false;
+            }
+        }
+
+        $query['params'] = $this->cloneVar($query['params']);
+
+        return $query;
+    }
+
+    /**
+     * Sanitizes a param.
+     *
+     * The return value is an array with the sanitized value and a boolean
+     * indicating if the original value was kept (allowing to use the sanitized
+     * value to explain the query).
+     */
+    private function sanitizeParam($var, ?\Throwable $error): array
+    {
+        if (\is_object($var)) {
+            return [$o = new ObjectParameter($var, $error), false, $o->isStringable() && !$error];
+        }
+
+        if ($error) {
+            return ['⚠ '.$error->getMessage(), false, false];
+        }
+
+        if (\is_array($var)) {
+            $a = [];
+            $explainable = $runnable = true;
+            foreach ($var as $k => $v) {
+                list($value, $e, $r) = $this->sanitizeParam($v, null);
+                $explainable = $explainable && $e;
+                $runnable = $runnable && $r;
+                $a[$k] = $value;
+            }
+
+            return [$a, $explainable, $runnable];
+        }
+
+        if (\is_resource($var)) {
+            return [sprintf('/* Resource(%s) */', get_resource_type($var)), false, false];
+        }
+
+        return [$var, true, true];
     }
 }
